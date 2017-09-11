665256f44614e3f3f552d413289b3121e3714c42
imosudi <imosudi@gmail.com>
2017-09-11 16:49:11 +0000

    Initial Commit- Views Mon Sep 11 16:49:11 UTC 2017

--- nextCashPoint/app.py
+++ nextCashPoint/app.py
@@ -4,7 +4,15 @@ from flask_moment import Moment
 from datetime import datetime
 
 
+from nextCashPoint.views.main import bp as index_bp
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+app.register_blueprint(index_bp)
+
+
+
+
