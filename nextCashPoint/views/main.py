e3ee707676c32da990c812c097119474705bd473
imosudi <imosudi@gmail.com>
2017-09-11 18:30:15 +0000

    Initail Commit -Views Mon Sep 11 18:30:15 UTC 2017

--- nextCashPoint/views/main.py
+++ nextCashPoint/views/main.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, url_for
+from datetime import datetime
 
 bp = Blueprint(__name__, __name__,template_folder='templates')
 
 @bp.route('/')
 def index():
-   return "Hello world!"
+   return render_templates('index.html', current_time=datetime.utcnow())
